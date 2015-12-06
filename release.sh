15a3d5c85f22f177f4ceb555bebd7f12d76038a2
Jin Chengxin <jcx631678546@gmail.com>
2015-12-06 20:11:34 +0800

    发布脚本修改完毕

--- release.sh
+++ release.sh
@@ -1,23 +1,48 @@
 #!/bin/sh
 
-if [[ $# -gt 1 ]]
+if [[ $# -gt 4 || $# -lt 1 ]]
 then
-	echo "release.sh <title>"
+	echo "Missing parameters!"
+	echo "release.sh <title> <keystore> <password> <password>"
 	exit 1
 fi
 
-export ANDROID_HOME=/Users/zengjin/sdk
+keystore=$2
 
-cd app
+cd ~/Desktop
+if [ ! -f "$keystore" ]; then
+	echo "keystore file not exist!"	
+	cd -
+	exit 1
+fi
+cd -
+
 gradle clean assembleRelease
-cd build/outputs/apk
-apktool decode -o app-release-unaligned.apk
+
+echo "gradle assemble end!"
+
+cd app/build/outputs/apk
+
+echo "begin to use apktool decode the apk"
+
+apktool decode -o decode app-release-unsigned.apk
 
 titles=(`echo "$1" | tr "," "\n"`)
 
+password1=$3
+password2=$4
+
+
 for title in "${titles[@]}"
 do
-	sed 's/android:label="[^"]*"/android:label="'"$title"'"/g' app/AndroidManifest.xml > app/AndroidManifest.xml.new
-	mv app/AndroidManifest.xml.new app/AndroidManifest.xml
-	apktool build -o app_${title}.apk app
+	sed 's/android:label="[^"]*"/android:label="'"$title"'"/g' decode/AndroidManifest.xml > decode/AndroidManifest.xml.new
+	mv decode/AndroidManifest.xml.new decode/AndroidManifest.xml
+	apktool build -o app_${title}.apk decode
+	jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ~/Desktop/$keystore -storepass $password1 -keypass $password2 app_${title}.apk $keystore
+	jarsigner -verify app_${title}.apk
+	zipalign -v 4 app_${title}.apk app_${title}-signed.apk
 done
+
+
+rm -r decode
+rm app-release-unsigned.apk
